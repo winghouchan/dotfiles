0baba512fc0834f2f2f15da50fee101151932599
Wing-Hou Chan <wing@wii.ng>
2022-02-09 19:53:26 +0000

    fish: Update logic commands to logic combiners

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,16 +1,16 @@
 if status is-interactive
     # Set up Homebrew environment if Homebrew is installed but not set up
-    if not command -v brew && test -x /opt/homebrew/bin/brew
+    if ! command -v brew && test -x /opt/homebrew/bin/brew
         source (/opt/homebrew/bin/brew shellenv | psub)
     end
 
     # Install and run Fish package manager and plugins if not installed
-    if not functions -q fisher
-        curl -sL https://git.io/fisher | source; and fisher update
+    if ! functions -q fisher
+        curl -sL https://git.io/fisher | source && fisher update
     end
 
     # Install Fast Node Manager if not installed
-    if not command -v fnm
+    if ! command -v fnm
         curl -fsSL https://fnm.vercel.app/install | bash
     end
 end
