c666a345e5df7582f8cfdca5bfe8c0cad0b83196
Wing-Hou Chan <wing@wii.ng>
2024-03-08 17:15:27 +0000

    config(terminal): configure hyper theme
    Set to "Material, Ocean".

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -127,6 +127,11 @@ module.exports = {
     // set to true to disable auto updates
     disableAutoUpdates: false,
     // for advanced config flags please refer to https://hyper.is/#cfg
+
+    // see https://hyper.is/store/hyper-material-theme/source?README.md
+    MaterialTheme: {
+      theme: "Ocean",
+    },
   },
   // a list of plugins to fetch and install from npm
   // format: [@org/]project[#version]
@@ -134,7 +139,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ["hyper-material-theme"],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
